ddfcf8bc09c3b910b50da855a740d1b5d20ec713
Addis Dittebrandt <addis.dittebrandt@kit.edu>
2026-01-31 10:16:28 +0100

    roid pickup

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float roidPickupRadius;
+
+    private HealthManager healthManager;
+
     public void onHealthChange(float health, float delta)
     {
     }
@@ -14,11 +18,25 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        healthManager = GetComponent<HealthManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var res = Physics.OverlapSphere(transform.position, roidPickupRadius);
+        foreach (var col in res)
+        {
+            var roid = col.GetComponent<Roid>();
+            if (roid == null)
+                continue;
+            healthManager.ChangeHealth(roid.healthAmount);
+            roid.PickUp();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, roidPickupRadius);        
     }
 }
