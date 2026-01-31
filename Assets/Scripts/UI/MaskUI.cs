41365ee53a46ff0a135885bde200b2a978f9e82f
Addis Dittebrandt <addis.dittebrandt@kit.edu>
2026-01-31 21:51:57 +0100

    fix MaskUI initialization

--- Assets/Scripts/UI/MaskUI.cs
+++ Assets/Scripts/UI/MaskUI.cs
@@ -8,17 +8,23 @@ public class MaskUI : MonoBehaviour
     private Image image3;
     private Image image4;
 
+    private bool init;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (init) return;
         image1 = transform.Find("Part1").GetComponent<Image>();
         image2 = transform.Find("Part2").GetComponent<Image>();
         image3 = transform.Find("Part3").GetComponent<Image>();
         image4 = transform.Find("Part4").GetComponent<Image>();
+        init = true;
     }
 
     public void SetCount(int count)
     {
+        Start();
+
         image1.enabled = count >= 1;
         image2.enabled = count >= 2;
         image3.enabled = count >= 3;
