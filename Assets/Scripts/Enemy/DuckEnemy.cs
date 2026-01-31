9743a3a06d0f2ea07bd7438484662b7db01cd516
Addis Dittebrandt <addis.dittebrandt@kit.edu>
2026-01-31 12:36:59 +0100

    fix duck enemy not getting destroyed

--- Assets/Scripts/Enemy/DuckEnemy.cs
+++ Assets/Scripts/Enemy/DuckEnemy.cs
@@ -77,6 +77,7 @@ public class EnemyBase : MonoBehaviour
             {
                 Destroy(gameObject);
             }
+            return;
         }
 
         var player = GameObject.FindGameObjectWithTag("Player");
