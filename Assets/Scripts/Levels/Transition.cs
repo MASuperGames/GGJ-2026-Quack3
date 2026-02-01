94724a695b406a7506fb0f2c8217ac5001ad677b
Addis Dittebrandt <addis.dittebrandt@kit.edu>
2026-02-01 11:20:27 +0100

    add level exit to playground into boss arena add trigger to player for level transitions

---
+++ Assets/Scripts/Levels/Transition.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            SceneManager.LoadScene(3);
+    }
+}
